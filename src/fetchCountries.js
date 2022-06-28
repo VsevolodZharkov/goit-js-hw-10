dbf45f55094536cff92b0e6d860a3d262d009106
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-06-28 18:44:23 +0300

    z

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -7,9 +7,8 @@ export default function fetchCountries(name) {
       return response.json();
     })
     .then(data => {
-			return fetch(`https://restcountries.com/v2/${data}?name=name.official,capital,population`)
-			.then(resorse => console.log(resorse))
-			.catch(error => console.log(error))
+			data
+			.then(data => console.log(data))
 			// // https://restcountries.com/v2/all?=name,capital,currencies
 			// console.log(data[0])
 		})
