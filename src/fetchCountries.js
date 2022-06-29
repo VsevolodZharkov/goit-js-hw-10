85bbe0ecee44c71c7266f0ff36dcc8efc6e3ba8e
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-06-29 08:12:33 +0300

    save

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,3 @@
-import Notiflix from 'notiflix';
-import { renderMarcup } from './renderMarcup';
-import { renderList } from './renderListInput';
-
-const ul = document.querySelector('.country-list');
 const fetchCountries = name => {
   return fetch(
     `https://restcountries.com/v2/name/${name}?name.official,capital,population,languages,flags.svg`
@@ -13,28 +8,7 @@ const fetchCountries = name => {
       }
       return response.json();
     })
-    .then(data => {
-      console.log(data.length);
-      console.log(data);
-      if (data.length > 10) {
-        Notiflix.Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-        return;
-      }
-      if (data.length >= 2 || data.length <= 10) {
-        renderList(data);
-        if (data.length === 1) {
-          ul.innerHTML = '';
-          const elem = data[0];
-          renderMarcup(elem);
-        }
-      }
-    })
-    .catch(error => {
-      console.log(error);
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-    });
+    .catch(error => console.log(error));
 };
 
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import { fetchCountries } from './fetchCountries';
 import './css/styles.css';
 import { debounce } from 'debounce';
+import Notiflix from 'notiflix';
+import { renderMarcup } from './renderMarcup';
+import { renderList } from './renderListInput';
 
-// const searchParams = new URLSearchParams({
-//   name: name.official,
-//   // capital: capital[0],
-// 	// population,
-// 	// flag: flags.svg,
-// 	// languages: languages.value
-// });
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box');
 const ul = document.querySelector('.country-list');
@@ -21,5 +17,27 @@ function inputHandler(e) {
   divInfo.innerHTML = '';
   const valueINp = e.target.value;
   let elem = valueINp.trim();
-  fetchCountries(elem);
+  fetchCountries(elem)
+    .then(data => {
+      console.log(data.length);
+      console.log(data);
+      if (data.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      if (data.length >= 2 || data.length <= 10) {
+        renderList(data);
+        if (data.length === 1) {
+          ul.innerHTML = '';
+          const elem = data[0];
+          renderMarcup(elem);
+        }
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
 }
